203e16288b5bd146bf25097bd2d7110ebea423c5
Gabriel Caraballo <gcaraballoperuapps@gmail.com>
2025-03-23 02:21:42 -0500

    add: services for change passsword

--- Application/DTOs/Auth/LoginResponse.cs
+++ Application/DTOs/Auth/LoginResponse.cs
@@ -11,5 +11,6 @@ namespace Application.DTOs.Auth
         public UserRole Role { get; set; }
         public string Token { get; set; }
         public string StudentId { get; set; }
+        public bool HasChangedPassword { get; set; }
     }
 } 
